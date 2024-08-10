6827b83ff29dfbce9318749dcebc13b5a9c6ac58
AnnaP-G <klientkv1511@gmail.com>
2024-08-10 17:56:10 +0300

    update CamperDetails

--- src/components/CamperDetails/CamperDetails.jsx
+++ src/components/CamperDetails/CamperDetails.jsx
@@ -10,37 +10,120 @@ const CamperDetails = ({ details }) => {
     TV,
     CD,
     radio,
-    shower,
     toilet,
     freezer,
     hob,
     microwave,
     gas,
-    water,
   } = details;
 
   return (
     <div className={css.detailsContainer}>
       <ul className={css.detailsList}>
-        <li className={css.detailsItem}>
-          {/* <svg className={css.svgHeart}>
-            <use href={`${symbolDefs}#icon-heart`} />
-          </svg> */}
-          Air Conditioner: {airConditioner}
-        </li>
-        <li className={css.detailsItem}>Bathroom: {bathroom}</li>
-        <li className={css.detailsItem}>Kitchen: {kitchen}</li>
-        <li className={css.detailsItem}>Beds: {beds}</li>
-        <li className={css.detailsItem}>TV: {TV}</li>
-        <li className={css.detailsItem}>CD: {CD}</li>
-        <li className={css.detailsItem}>Radio: {radio}</li>
-        <li className={css.detailsItem}>Shower: {shower}</li>
-        <li className={css.detailsItem}>Toilet: {toilet}</li>
-        <li className={css.detailsItem}>Freezer: {freezer}</li>
-        <li className={css.detailsItem}>Hob: {hob}</li>
-        <li className={css.detailsItem}>Microwave: {microwave}</li>
-        <li className={css.detailsItem}>Gas: {gas}</li>
-        <li className={css.detailsItem}>Water: {water}</li>
+        {airConditioner > 0 && (
+          <li className={css.detailsItem}>
+            <svg className={css.svg}>
+              <use
+                href={`${symbolDefs}#icon-hotel-air`}
+                width="20"
+                height="20"
+              />
+            </svg>
+            {airConditioner} Air conditioner
+          </li>
+        )}
+        {bathroom > 0 && (
+          <li className={css.detailsItem}>
+            <svg className={css.svg}>
+              <use href={`${symbolDefs}#icon-shower`} width="20" height="20" />
+            </svg>
+            {bathroom} Shower
+          </li>
+        )}
+        {kitchen > 0 && (
+          <li className={css.detailsItem}>
+            <svg className={css.svg}>
+              <use href={`${symbolDefs}#icon-kitchen`} width="20" height="20" />
+            </svg>
+            {kitchen} Kitchen
+          </li>
+        )}
+        {beds > 0 && (
+          <li className={css.detailsItem}>
+            <svg className={css.svg}>
+              <use href={`${symbolDefs}#icon-bed`} width="20" height="20" />
+            </svg>
+            {beds} Beds
+          </li>
+        )}
+        {TV > 0 && (
+          <li className={css.detailsItem}>
+            <svg className={css.svg}>
+              <use href={`${symbolDefs}#icon-TV`} width="20" height="20" />
+            </svg>
+            {TV} TV
+          </li>
+        )}
+        {CD > 0 && (
+          <li className={css.detailsItem}>
+            <svg className={css.svg}>
+              <use href={`${symbolDefs}#icon-cd`} width="20" height="20" />
+            </svg>
+            {CD} CD
+          </li>
+        )}
+        {radio > 0 && (
+          <li className={css.detailsItem}>
+            <svg className={css.svg}>
+              <use href={`${symbolDefs}#icon-radio`} width="20" height="20" />
+            </svg>
+            {radio} Radio
+          </li>
+        )}
+        {toilet > 0 && (
+          <li className={css.detailsItem}>
+            <svg className={css.svg}>
+              <use href={`${symbolDefs}#icon-toilet`} width="20" height="20" />
+            </svg>
+            {toilet} Toilet
+          </li>
+        )}
+        {freezer > 0 && (
+          <li className={css.detailsItem}>
+            <svg className={css.svg}>
+              <use href={`${symbolDefs}#icon-freezer`} width="20" height="20" />
+            </svg>
+            {freezer} Freezer
+          </li>
+        )}
+        {hob > 0 && (
+          <li className={css.detailsItem}>
+            <svg className={css.svg}>
+              <use href={`${symbolDefs}#icon-hob`} width="20" height="20" />
+            </svg>
+            {hob} Hob
+          </li>
+        )}
+        {microwave > 0 && (
+          <li className={css.detailsItem}>
+            <svg className={css.svg}>
+              <use
+                href={`${symbolDefs}#icon-microwave`}
+                width="20"
+                height="20"
+              />
+            </svg>
+            {microwave} Microwave
+          </li>
+        )}
+        {gas > 0 && (
+          <li className={css.detailsItem}>
+            <svg className={css.svg}>
+              <use href={`${symbolDefs}#icon-gas`} width="20" height="20" />
+            </svg>
+            {gas} Gas
+          </li>
+        )}
       </ul>
     </div>
   );
